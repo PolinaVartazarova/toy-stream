119aa9b544b013e48eec7c7aa72ad511a9c19b8a
Polina Vartazarova <polinavartazarova@gmail.com>
2021-08-02 23:32:20 +0300

    Добавляет новую шапку и выпадающее меню

--- js/script.js
+++ js/script.js
@@ -1,16 +1,9 @@
-(() => {
-const menuBtnRef = document.querySelector("[data-menu-button");
-const mobileMenuRef = document.querySelector("[data-menu");
-
-menuBtnRef.addEventListener("click", () => {
-	const expanded =
-	   menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
-
-    menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
-
-
-    mobileMenuRef.classList.toggle("is-open");
- });
-})();
+const iconMenu = document.querySelector('.header-burger');
+if (iconMenu){
+	const menuBody = document.querySelector('.nav');
+	iconMenu.addEventListener("click", function(e) {
+		document.body.classList.toggle('lock');
+		iconMenu.classList.toggle('active');
+		menuBody.classList.toggle('active');
+	});
+}

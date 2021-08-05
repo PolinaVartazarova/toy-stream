ea44c9450a169bb8826220d8dec8ea6e6905b493
Polina Vartazarova <polinavartazarova@gmail.com>
2021-08-05 22:59:42 +0300

    Добавляет базу для полноэкранной прокрутки

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Бургер-меню
+
 const iconMenu = document.querySelector('.header-burger');
 if (iconMenu){
 	const menuBody = document.querySelector('.nav');
@@ -7,3 +9,84 @@ if (iconMenu){
 		menuBody.classList.toggle('active');
 	});
 }
+
+
+
+// Слайдер для страницы "Карьера"
+let pageSlider = new Swiper('.page',{
+
+// Cвои классы
+	wrapperClass: "page-wrapper",
+	slideClass: "page-screen",
+
+
+// Вертикальный слайдер
+	direction: 'vertical',
+
+// Количество слайдов дял показа
+	slidesPerView: 'auto',
+
+// включение паралалкса
+	parallax: true,
+
+
+// Управление клавиатурой
+	keyboard: {
+          
+        // Вкл/выкл
+		enabled: true,
+
+		// Вкл/выкл Только когда слайдер в пределах вьюпорта
+		onlyInViewport: true,
+
+		// Вкл/выкл Управление клавишами
+		pageUpDown: true,
+	},
+
+// Управление колесом мыши
+    mousewheel: {
+
+    	// Чувствительность колеса мыши
+    	sensitivity: 1,
+    	// Класс объекта на котором будет срабатывать прокрутка мышью
+    	//eventsTarget: ".block-blue"
+    	//eventsTarget: ".vacancy-block"
+    },
+
+    // Отключение функционала если слайдов меньше чем нужно
+    watchOverflow: true,
+
+    // Скорость
+    speed: 800,
+
+    // Обновить слайдер при изменении элементов слайдера
+    observer: true,
+
+    // Обновить слайдер при изменении родительских элементов слайдера
+    observeParents: true,
+
+    // Обновить слайдер при изменении дочерних элементов слайда
+    observeSlideChildren: true,
+
+
+// Навигация
+   
+    // Буллеты, текущее положение, прогерсс-бар
+    pagination: {
+    	el: '.page-pagination',
+    	type: 'bullets',
+    	clickable: true,
+    	bulletClass: "page-bullet",
+    	bulletActiveClass: "page-bullet-active",
+
+   },
+
+   // Скролл
+    scrollbar: {
+    	el: '.page-scroll',
+    	dragClass: "page-drag-scroll",
+        // Возможность перетаскивать скролл
+        draggable: true
+   },
+
+})
